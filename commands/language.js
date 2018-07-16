474aed54041a2f723975d30051b5c47ec827b6bb
ImAndreLeandro <40339453+ImAndreLeandro@users.noreply.github.com>
2018-07-17 00:59:30 +0100

    Update language.js

--- commands/language.js
+++ commands/language.js
@@ -7,12 +7,15 @@ exports.run = async(client, message, args) => {
   var language = await db.fetch(`guild_language_${message.guild.id}`);
   if (language === null) language = 0;
   
- const nothing = new Discord.RichEmbed()
-    .setAuthor("Language", message.guild.iconURL)
-    .setColor([54, 57, 64])
-    .setDescription(`Current Language: **${utils.getLanguage(language)}**!`
-                   + `\n\nUse: \`${prefix}language <new_language>\``);
-  if (args.length === 0) return message.channel.send(nothing);
+  if (args.length === 0) {
+   const nothing = new Discord.RichEmbed()
+      .setAuthor("Language", message.guild.iconURL)
+      .setColor([54, 57, 64])
+      .setDescription(`Current Language: **${utils.getLanguage(language)}**!`
+                     + `\n\nUse: \`${prefix}language <new_language>\``);
+    message.channel.send(nothing);
+  }
+  
   if (args.length === 1) {
     let amount = parseInt(args[0]);
     if (Number.isInteger(amount)) {
