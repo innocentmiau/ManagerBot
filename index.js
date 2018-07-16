d26b3146e40b477605fe4f5b922ecc7cc50d1eec
ImAndreLeandro <40339453+ImAndreLeandro@users.noreply.github.com>
2018-07-16 23:44:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-test
+const Discord = require("discord.js");
+const config = require("./config.json");
+
+const client = new Discord.Client();
+
+client.on("ready", () => {
+	console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+	client.user.setActivity(`jb!help | ${client.guilds.size} servers!`, {type: 'Playing'});
+});
+
+client.login(process.env.TOKEN);
