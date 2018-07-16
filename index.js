9b69b8f8b2c58ca81589eb7000c7cc09d2c9e061
ImAndreLeandro <40339453+ImAndreLeandro@users.noreply.github.com>
2018-07-16 23:59:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,25 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
 	console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-	client.user.setActivity(`jb!help | ${client.guilds.size} servers!`, {type: 'Playing'});
+	client.user.setActivity(`mb!help | ${client.guilds.size} servers!`, {type: 'Playing'});
 });
+
+client.on("message", async message => {
+	let msg = message.content.toLowerCase();
+	if (message.author.bot) return undefined;
+	
+	if (message.content.indexOf(config.prefix) !== 0) return;
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
+	
+	try {
+		let commands = require(`./commands/${command}.js`);
+		commands.run(client, message, args);
+	} catch(e) {
+		console.log(e);
+	} finally {
+	}
+	
+}
 
 client.login(process.env.TOKEN);
