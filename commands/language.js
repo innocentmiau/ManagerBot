0e78a35ff6098487d0cd5085d0d9880576477532
ImAndreLeandro <40339453+ImAndreLeandro@users.noreply.github.com>
2018-07-17 00:46:41 +0100

    Update language.js

--- commands/language.js
+++ commands/language.js
@@ -1,15 +1,17 @@
 const Discord = require('discord.js');
 const db = require('quick.db');
+const utils = require("../utils.js");
 
 exports.run = async(client, message, args) => {
   const prefix = "mb!";
   var language = await db.fetch(`guild_language_${message.guild.id}`);
   if (language === null) language = 0;
+  const 
   
   const nothing = new Discord.RichEmbed()
     .setAuthor("Language", message.guild.iconURL)
     .setColor([54, 57, 64])
-    .setDescription(`Current Language: **${language}**!`
+    .setDescription(`Current Language: **${utils.getLanguage(language)}**!`
                    + `\n\nUse: \`${prefix}language <new_language>\``);
   if (args.length === 0) return message.channel.send(nothing);
 }
