d0810afce1436a33c8d0aead5cb226dacce3e7aa
ImAndreLeandro <40339453+ImAndreLeandro@users.noreply.github.com>
2018-07-17 23:52:35 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -12,6 +12,7 @@ exports.run = async (client, message, args) => {
       .setColor([54, 57, 64])
       .setDescription(`Pong! :ping_pong: \nLatency: **${m.createdTimestamp - message.createdTimestamp}**ms.\nAPI Latency: **${Math.round(client.ping)}**ms.`)
       .setTimestamp();
+    m.edit(embed);
   } else if (language === 1) {
       const m = await message.channel.send("A Calcular...");
       let embed = new Discord.RichEmbed()
@@ -19,6 +20,6 @@ exports.run = async (client, message, args) => {
         .setColor([54, 57, 64])
         .setDescription(`Pong! :ping_pong: \nLatencia: **${m.createdTimestamp - message.createdTimestamp}**ms.\nLatencia do API: **${Math.round(client.ping)}**ms.`)
         .setTimestamp();
+      m.edit(embed);
     }
-  m.edit(embed);
 }
