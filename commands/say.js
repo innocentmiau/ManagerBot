b3a4352d42c5249210396907fef11e4f94d5f1f1
ImAndreLeandro <40339453+ImAndreLeandro@users.noreply.github.com>
2018-07-18 00:22:56 +0100

    Create say.js

---
+++ commands/say.js
@@ -0,0 +1,13 @@
+exports.run = (client, message, args) => {
+    if (message.author.id === "197340056053219329" || message.author.id === "334731457945796609") {
+      if (args.length === 0) {
+        message.channel.send("**" + message.author.username + "**, use: `jb!say <message>`");
+      } else {
+        let say = args.join(' ');
+        message.delete();
+        message.channel.send(say);
+      } 
+    } else {
+      message.channel.send("**" + message.author.username + "**, I'm sorry but you need to be the bot owner!");
+    }
+}
