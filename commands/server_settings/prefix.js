6d853030c9ba37559832b08d1950c054002ecf6e
Diogo "DikaN" Cândido <diogomiguelcandido@gmail.com>
2018-07-17 01:44:31 +0100

    Updated languages and bugs in prefix.js

--- commands/server_settings/prefix.js
+++ commands/server_settings/prefix.js
@@ -1,10 +1,23 @@
 const Discord = require('discord.js');
+const db = require('quick.db');
+const utils = require("../utils.js");
 
 exports.run = async (client, message, args) => {
-  const embed = new Discord.RichEmbed()
-    .setAuthor("Prefix")
-    .setDescription("My prefix in this server: **jb!**.")
-    .setColor([54, 57, 64])
-    .setTimestamp();
-  message.channel.send(embed);
+  var language = await db.fetch(`guild_language_${message.guild.id}`);
+  if (language === null) language = 0;
+  if (language === 0) {
+    const embed = new Discord.RichEmbed()
+      .setAuthor("Prefix")
+      .setDescription("My prefix is: **mb!**.")
+      .setColor([54, 57, 64])
+      .setTimestamp();
+    message.channel.send(embed);
+  } else if (language === 1) {
+      const embed = new Discord.RichEmbed()
+        .setAuthor("Prefix")
+        .setDescription("O meu prefix é: **mb!**.")
+        .setColor([54, 57, 64])
+        .setTimestamp();
+      message.channel.send(embed);
+    }
 }
