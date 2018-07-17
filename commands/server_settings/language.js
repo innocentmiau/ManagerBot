ece7517bc1f8717a6b778369a8cc6fc534f9e885
ImAndreLeandro <40339453+ImAndreLeandro@users.noreply.github.com>
2018-07-18 00:03:59 +0100

    Update language.js

--- commands/server_settings/language.js
+++ commands/server_settings/language.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const utils = require("./../utils.js");
+const utils = require("../../utils.js");
 
 exports.run = async(client, message, args) => {
   const prefix = client.prefix;
