d915c1b5fb895f304177476bbf26716358aa395d
Diogo "DikaN" Cândido <diogomiguelcandido@gmail.com>
2018-07-17 01:31:42 +0100

    Updated Languages

--- commands/ping.js
+++ commands/ping.js
@@ -2,16 +2,23 @@ const Discord = require('discord.js');
 const db = require('quick.db');
 const utils = require("../utils.js");
 
-var language = await db.fetch(`guild_language_${message.guild.id}`);
-if (language === null) language = 0;
-
-exports.run = async (client, message, args) => {
-  const m = await message.channel.send("Calculating...");
-  let embed = new Discord.RichEmbed()
-    .setAuthor("Latency!", client.user.displayAvatarURL)
-    .setColor([54, 57, 64])
-    .setDescription(`Pong! :ping_pong: \nLatency is **${m.createdTimestamp - message.createdTimestamp}**ms.\nAPI Latency is **${Math.round(client.ping)}**ms.`)
-    .setTimestamp();
-
+exports.run = async (client, message, args) => { 
+  var language = await db.fetch(`guild_language_${message.guild.id}`);
+  if (language === null) language = 0;
+  if (language === 0) {
+    const m = await message.channel.send("Calculating...");
+    let embed = new Discord.RichEmbed()
+      .setAuthor("Latency!", client.user.displayAvatarURL)
+      .setColor([54, 57, 64])
+      .setDescription(`Pong! :ping_pong: \nLatency: **${m.createdTimestamp - message.createdTimestamp}**ms.\nAPI Latency: **${Math.round(client.ping)}**ms.`)
+      .setTimestamp();
+  } else if (language === 1) {
+      const m = await message.channel.send("A Calcular...");
+      let embed = new Discord.RichEmbed()
+        .setAuthor("Latencia!", client.user.displayAvatarURL)
+        .setColor([54, 57, 64])
+        .setDescription(`Pong! :ping_pong: \nLatencia: **${m.createdTimestamp - message.createdTimestamp}**ms.\nLatencia do API: **${Math.round(client.ping)}**ms.`)
+        .setTimestamp();
+    }
   m.edit(embed);
 }
