ff82ac18f14d343debf63709a9bc3ef4547e31cc
Diogo "DikaN" Cândido <diogomiguelcandido@gmail.com>
2018-07-18 00:35:10 +0100

    Create bash.js

---
+++ commands/sources/bash.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+
+exports.run = async (client, message, args) => {
+  const input = args.join(" ");
+  const embed = new Discord.RichEmbed()
+    .setDescription("Sent by " + message.author.username + "" + 
+                    \``bash\n+ input +````)
+    .setAuthor("Bash", "https://i.imgur.com/q5q5q4F.png")
+    .setColor([54, 57, 64])
+    .setTimestamp();
+  message.channel.send(embed);
+  message.delete();
+}
