4a9a86e774a6e058547e51aa443e58e28d8ebd9c
ImAndreLeandro <40339453+ImAndreLeandro@users.noreply.github.com>
2018-07-17 01:03:25 +0100

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,11 @@
+exports.run = async (client, message, args) => {
+	const m = await message.channel.send("Calculating...");
+  const Discord = require('discord.js');
+  let embed = new Discord.RichEmbed()
+    .setAuthor("Latency!")
+    .setColor([54, 57, 64])
+    .setDescription(`Pong! :ping_pong: \nLatency is **${m.createdTimestamp - message.createdTimestamp}**ms.\nAPI Latency is **${Math.round(client.ping)}**ms.`)
+    .setTimestamp();
+
+  m.edit(embed);
+}
